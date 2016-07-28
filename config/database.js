426320d9c7e816723cee76b78b4c12be0f36d8a1
antbrothers <jianxi_lin@163.com>
2016-07-28 09:16:31 +0800

    连接数据库的基类

---
+++ config/database.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2016/7/5.
+ */
+
+module.exports={
+    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    localUrl: 'mongodb://localhost/meanstacktutorials'
+}
